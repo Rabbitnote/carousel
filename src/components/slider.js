7f8a98b3421f968862ce27da9e4d2fcf82b63504
note <note16651@hotmail.co.th>
2021-10-28 14:22:09 +0700

    [FIX] Style and adding function to arrow

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,26 +1,56 @@
 import React, { useState } from 'react';
-import SliderContent from './sliderContent';
 import '../Styles/slider.css';
 import Slide from './slide';
 import Arrow from './Arrow';
 
 const Slider = props => {
     const getWidth = () => window.innerWidth;
-    const [state, setState] = useState({ translate: 0, transition: 0.45 });
-    const { translate, transition } = state;
+    const [state, setState] = useState({
+        translate: 0,
+        transition: 0.45,
+        activeIndex: 0
+    });
+    const { translate, transition, activeIndex } = state;
+    const nextSlide = () => {
+        if (activeIndex === props.slides.length - 1) {
+            return setState({ ...state, translate: 0, activeIndex: 0 });
+        }
+        setState({
+            ...state,
+            activeIndex: activeIndex + 1,
+            translate: (activeIndex + 1) * getWidth()
+        });
+    };
+    const prevSlide = () => {
+        if (activeIndex === 0) {
+            return setState({
+                ...state,
+                activeIndex: props.slides.length - 1,
+                translate: (props.slides.length - 1) * getWidth()
+            });
+        }
+        setState({
+            ...state,
+            activeIndex: activeIndex - 1,
+            translate: (activeIndex - 1) * getWidth()
+        });
+    };
     return (
         <div className='container'>
-            <SliderContent
-                translate={translate}
-                transition={transition}
-                width={getWidth() * props.slides.length}
+            <div
+                className='sliderContent'
+                style={{
+                    transform: `translateX(-${translate}px)`,
+                    transition: `transform ease-out ${transition}s`,
+                    width: `${getWidth() * props.slides.length}px`
+                }}
             >
                 {props.slides.map(slide => {
                     return <Slide key={slide} content={slide} />;
                 })}
-            </SliderContent>
-            <Arrow direction='left' />
-            <Arrow direction='right' />
+            </div>
+            <Arrow direction='left' handlerClick={prevSlide} />
+            <Arrow direction='right' handlerClick={nextSlide} />
         </div>
     );
 };
